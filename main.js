6ae7885a7e0f5cc29ebc3359038bb01737309021
bielberg <101133918+bielberg@users.noreply.github.com>
2022-03-07 21:19:39 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 
+
 let gender
 const button = document.querySelector('.button');
 const text = document.querySelector('.inp');
-
-const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
-const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
-
+const serverUrl = 'https://api.genderize.io';
 
 button.addEventListener('click', function(){
     if (text.value){
-        const cityName = text.value;
-        const url = `${serverUrl}?q=${cityName}&appid=${apiKey}`;
+
+        const url = `${serverUrl}?name=${text.value}`;
         gender = fetch(url)
         .then(function(response){
             return response.json();
